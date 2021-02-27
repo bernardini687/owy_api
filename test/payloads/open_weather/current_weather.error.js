af9068d0805f737e8f1aadfdbd0cc05713621809
oscar bernardini <bernardini687@gmail.com>
2021-02-27 09:22:14 +0100

    refactor: mock actual responses objects

--- test/payloads/open_weather/current_weather.error.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  cod: 401,
-  message: 'Invalid API key. Please see http://openweathermap.org/faq#error401 for more info.'
-}
---
+++ test/responses/open_weather/current_weather/error.js
@@ -0,0 +1,8 @@
+module.exports = {
+  status: 401,
+  statusText: 'Unauthorized',
+  data: {
+    cod: 401,
+    message: 'Invalid API key. Please see http://openweathermap.org/faq#error401 for more info.'
+  }
+}
